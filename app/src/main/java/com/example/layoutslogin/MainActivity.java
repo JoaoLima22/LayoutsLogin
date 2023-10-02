7a1f9dfb9a6443e5e3069f4b44915b7359a213ea
Joao <joaovitorlimacastroteixeira@gmail.com>
2023-10-02 16:01:39 -0300

    Initial commit

---
+++ app/src/main/java/com/example/layoutslogin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.layoutslogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ll_login);
+    }
+}
